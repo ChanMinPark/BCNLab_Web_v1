df34dd32c73ac96e01cf4141714af674a2c101cf
ChanMinPark <walkinpcm@gmail.com>
2016-05-10 22:46:56 +0900

    add page

---
+++ BCNLab_v1/src/main/java/com/bcnlab/web/ProfessorIntroController.java
@@ -0,0 +1,22 @@
+package com.bcnlab.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProfessorIntroController {
+private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping(value = "/professor")
+	public String home(Model model) {
+		logger.info("Professor Introduction Page.");
+		
+		
+		//model.addAttribute("serverTime", formattedDate );
+		
+		return "professorIntro";
+	}
+}
