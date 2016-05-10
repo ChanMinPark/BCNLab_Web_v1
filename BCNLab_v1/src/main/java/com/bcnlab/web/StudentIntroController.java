e694e2b45230ff3a30219f348efea006f4c149a6
ChanMinPark <walkinpcm@gmail.com>
2016-05-10 23:03:35 +0900

    modify Controller

--- BCNLab_v1/src/main/java/com/bcnlab/web/StudentIntroController.java
+++ BCNLab_v1/src/main/java/com/bcnlab/web/StudentIntroController.java
@@ -2,9 +2,11 @@ package com.bcnlab.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class StudentIntroController {
 	private static final Logger logger = LoggerFactory.getLogger(StudentIntroController.class);
 
