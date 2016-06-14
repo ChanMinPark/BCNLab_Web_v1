8e5314ad3c98162ef2372d3041a5da63004d0c17
ChanMinPark <walkinpcm@gmail.com>
2016-06-14 23:13:24 +0900

    Update Board content.

--- BCNLab_v1/src/main/java/com/bcnlab/web/controller/BoardController.java
+++ BCNLab_v1/src/main/java/com/bcnlab/web/controller/BoardController.java
@@ -17,7 +17,7 @@ public class BoardController {
 	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
 
 	@RequestMapping(value = "/board/news", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String news(Locale locale, Model model) {
 		logger.info("Board/news Page.");
 		
 		Date date = new Date();
@@ -28,6 +28,34 @@ public class BoardController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "board_news";
+	}
+	
+	@RequestMapping(value = "/board/trend", method = RequestMethod.GET)
+	public String trend(Locale locale, Model model) {
+		logger.info("Board/trend Page.");
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "board_trend";
+	}
+	
+	@RequestMapping(value = "/board/assignment ", method = RequestMethod.GET)
+	public String assignment(Locale locale, Model model) {
+		logger.info("Board/assignment  Page.");
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "board_assignment";
 	}
 	
 }
